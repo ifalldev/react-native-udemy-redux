04204f5477594d5a29e0df24c373f715cbf8b468
ifalldev <cbatista.93@gmail.com>
2017-01-23 02:16:51 -0200

    import components; beginning with redux, provider/store first config

---
+++ src/components/LibraryList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { View } from 'react-native';
+
+class LibraryList extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <View />
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
